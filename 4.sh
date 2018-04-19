8d9b6c1d7da8061bd17fe8c5efe72b7d058d7714
ism47597075 <ism47597075@a03.informatica.escoladeltreball.org>
2018-04-19 09:22:54 +0200

    2

---
+++ 4.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+# Filename:        script4.sh
+# Author:          Coral, Daniel  (ism47597075)
+# Date:            12/4/2018
+# Version:         0.1
+# License:         This is free software, licensed under the GNU General Public License v3.
+#                  See http://www.gnu.org/licenses/gpl.html for more information.
+# Usage:           sh script4.sh
+
+# Description:    Mostra els usuaris conectats, els no conectats i els inexistents.
+
+echo "Introdueix l'usuari: "
+read user
+
+if [[ -z $(cat /etc/passwd |grep -v $user) ]]
+then
+	echo "L'usuari no existeix"
+
+elif [[ -n $(who |grep $user) ]]
+then
+	echo "L'usuari esta conectat."
+
+else
+	echo "L'usuari no esta conectat."
+fi
--- script4.sh
+++ script4.sh
@@ -1,22 +1,8 @@
-#/bin/bash
-# Filename:        script4.sh
-# Author:          Coral, Daniel  (ism47597075)
-# Date:            12/4/2018
-# Version:         0.1
-# License:         This is free software, licensed under the GNU General Public License v3.
-#                  See http://www.gnu.org/licenses/gpl.html for more information.
-# Usage:           sh script4.sh
-
-# Description:    Mostra els usuaris conectats, els no conectats i els inexistents.
-
+#!/bin/bash
 echo "Introdueix l'usuari: "
 read user
 
 while  [[ -z $(cat /etc/passwd |grep -v $user) ]]
-then
-	echo "L'usuari no existeix"
-
-
-
-
-
+do
+echo "L'usuari no existeix"
+done
